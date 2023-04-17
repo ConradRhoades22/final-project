c5510a0fb8522268f401198b15a39b9a7cb80307
Conrad Rhoades <conradrhoades@gmail.com>
2023-04-17 10:31:14 -0700

    inital commit

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const app = express()
+const morgan = require('morgan')
+const mongoose = require('mongoose')
+const {expressjwt} = require('express-jwt')
+
+app.use(express.json())
+app.use(morgan('dev'))
+
+mongoose.connect(
+    process.env.MONGO_URL,
+    () => console.log('Connected to the DB')
+)
+
+app.use('/auth', require('./routes/authRouter'))
+app.use('/api', expressjwt({ secret: process.env.SECRET, algorithms: ['HS256']}))
+app.use('/api/character', require('./routes/characterRouter'))
+
+app.use ((err, req, res, next) => {
+    console.log(err)
+    if(err.name === "UnauthorizedError"){
+        res.status(err.status)
+    }
+    return res.send({errMsg: err.message})
+})
+
+app.listen(9000, () => {
+    console.log("Server is running on local port 9000")
+})
